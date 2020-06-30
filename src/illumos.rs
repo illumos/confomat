166fcad4925b33e808ad37903806d0135e7306ab
Joshua M. Clulow <josh@sysmgr.org>
2020-06-30 05:16:11 +0000

    allow query of user_attr(4) entries by username

--- src/illumos.rs
+++ src/illumos.rs
@@ -2,10 +2,97 @@
  * Copyright 2020 Oxide Computer Company
  */
 
-use std::os::raw::c_char;
+use std::os::raw::{c_char, c_int};
 use std::process::exit;
 use std::ffi::{CString, CStr};
+use std::collections::HashMap;
 use anyhow::{Result, bail};
+
+#[derive(Debug, PartialEq)]
+pub struct UserAttr {
+    pub name: String,
+    pub attr: HashMap<String, String>,
+}
+
+impl UserAttr {
+    pub fn profiles(&self) -> Vec<String> {
+        if let Some(p) = self.attr.get("profiles") {
+            p.split(',')
+                .map(|s| s.trim().to_string())
+                .collect::<Vec<_>>()
+        } else {
+            Vec::new()
+        }
+    }
+}
+
+#[repr(C)]
+struct Kv {
+    key: *const c_char,
+    value: *const c_char,
+}
+
+impl Kv {
+    fn name(&self) -> &CStr {
+        unsafe { CStr::from_ptr(self.key) }
+    }
+
+    fn value(&self) -> &CStr {
+        unsafe { CStr::from_ptr(self.value) }
+    }
+}
+
+#[repr(C)]
+struct Kva {
+    length: c_int,
+    data: *const Kv,
+}
+
+impl Kva {
+    fn values(&self) -> &[Kv] {
+        unsafe { std::slice::from_raw_parts(self.data, self.length as usize) }
+    }
+}
+
+#[repr(C)]
+struct UserAttrRaw {
+    name: *mut c_char,
+    qualifier: *mut c_char,
+    res1: *mut c_char,
+    res2: *mut c_char,
+    attr: *mut Kva,
+}
+
+#[link(name = "secdb")]
+extern {
+    fn getusernam(buf: *const c_char) -> *mut UserAttrRaw;
+    fn free_userattr(userattr: *mut UserAttrRaw);
+}
+
+pub fn get_user_attr_by_name(name: &str) -> Result<Option<UserAttr>> {
+    let mut out = UserAttr {
+        name: name.to_string(),
+        attr: HashMap::new(),
+    };
+
+    let name = CString::new(name.to_owned())?;
+    let ua = unsafe { getusernam(name.as_ptr()) };
+    if ua.is_null() {
+        return Ok(None);
+    }
+
+    for kv in unsafe { (*(*ua).attr).values() } {
+        if let (Ok(k), Ok(v)) = (kv.name().to_str(), kv.value().to_str()) {
+            out.attr.insert(k.to_string(), v.to_string());
+        } else {
+            continue;
+        }
+    }
+
+    unsafe { free_userattr(ua) };
+
+    Ok(Some(out))
+}
 
 pub fn nodename() -> String {
     unsafe {
